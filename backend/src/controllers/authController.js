2c4967e6d3201a648f5459758a92f47149e5662a
Kyaw Zay Ya <zayya4281@gmail.com>
2026-07-20 00:14:09 +0630

    cookien setting change

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -15,9 +15,9 @@ const login = async (req, res) => {
         // Set cookie
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            secure: true,
+            sameSite: 'none',
+            maxAge: 3600000
         });
 
         Msg(res, 'Login successful', {
@@ -37,8 +37,9 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
     res.cookie('token', '', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: true,
+        sameSite: 'none',
+        expires: new Date(0),
     });
     Msg(res, 'Logged out successfully', {}, 200);
 };

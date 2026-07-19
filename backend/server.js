d525d20f0ff547acfd4da75cad8fbac85f6c6464
Kyaw Zay Ya <zayya4281@gmail.com>
2026-07-20 00:22:32 +0630

    fix: update CORS origin to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors(
     {
-        origin: process.env.CLIENT_URL || 'http://localhost:3000',
-        credentials: true
+        origin: process.env.CLIENT_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"]
     }));
 app.use(express.json());
 app.use(cookieParser());
